f646097ebecf7123e161765adc7acc60673ec10c
min_hein_khant <aungkoung007@gmail.com>
2020-04-28 03:11:37 +0630

    feat(theme-dev): Added Theme UI

--- packages/gatsby-theme-docs/src/components/layout.js
+++ packages/gatsby-theme-docs/src/components/layout.js
@@ -1,11 +1,19 @@
-import React from 'react';
+/** @jsx jsx */
+import { Fragment } from 'react';
+import { jsx } from 'theme-ui';
+import { Global } from '@emotion/core';
 
 const Layout = ({ children }) => {
   return (
-    <React.Fragment>
-      <header>gatsby-theme-docs</header>
-      <main>{children}</main>
-    </React.Fragment>
+    <Fragment>
+      <Global styles={{ body: { margin: 0 } }} />
+      <header
+        sx={{ bg: 'primary', color: 'background', fontFamily: 'heading', p: 3 }}
+      >
+        gatsby-theme-docs
+      </header>
+      <main sx={{ mx: 'auto', maxWidth: 650, width: '90vw' }}>{children}</main>
+    </Fragment>
   );
 };
 
